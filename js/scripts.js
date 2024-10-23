16c36db8bb8e55ff6c740662ba20b51e34f5956f
onepiece-coding <makclaaax@gmail.com>
2024-10-23 17:13:43 +0200

    Partners Section Added!

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ const toggleNavbarMenuBtn = document.querySelector(".toggle-navbar-menu");
 
 const navbarEl = document.querySelector(".navbar");
 
+// Toggle Mobile Navbar
+
 toggleNavbarMenuBtn.addEventListener("click", function () {
 
     if (navbarEl.classList.contains("show")) {
@@ -18,5 +20,55 @@ toggleNavbarMenuBtn.addEventListener("click", function () {
 
 
     }
+
+});
+
+// Initialize Swiper
+
+const swiper = new Swiper('.swiper', {
+
+    slidesPerView: 1,       // Number of slides visible at a time
+
+    spaceBetween: 20,       // Space between each slide (in pixels)
+
+    loop: true,             // Loop through slides
+
+    // pagination: {
+
+    //     el: '.swiper-pagination',
+
+    //     clickable: true,
+
+    // },
+
+    navigation: {
+
+        nextEl: '.swiper-button-next',
+
+        prevEl: '.swiper-button-prev',
+
+    },
+
+    breakpoints: { // Media Queries
+
+        640: {
+
+            slidesPerView: 1, // Display 1 slide at a time on smaller screens
+
+        },
+
+        768: {
+
+            slidesPerView: 2, // Display 2 slides on medium-sized screens
+
+        },
+
+        1024: {
+
+            slidesPerView: 3, // Display 3 slides on larger screens
+
+        },
+
+    },
 
 });
